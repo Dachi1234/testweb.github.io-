2c9355628b81c133c092676cf50668b83dc0a80d
Dachi1234 <71288515+Dachi1234@users.noreply.github.com>
2024-09-24 15:01:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function displayProducts(productsToDisplay) {
 // Fetch products from the backend API
 async function fetchProducts() {
   try {
-    const response = await fetch('https://testweb-github-io.onrender.com/products');
+    const response = await fetch('https://testweb-github-io.onrender.com/product');
     const data = await response.json();
     products = data; // Assign fetched data to products
     productNames = products.map(product => product.name); // Update productNames
