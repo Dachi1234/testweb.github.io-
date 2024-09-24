8591761e23252a524db1fe41e7ca8bc42e007946
Dachi1234 <71288515+Dachi1234@users.noreply.github.com>
2024-09-24 14:34:28 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Require cors package
 const app = express();
-const Product = require('./models/Product');
+const Product = require('Product');
 
 // Middleware to parse JSON bodies
 app.use(express.json());
